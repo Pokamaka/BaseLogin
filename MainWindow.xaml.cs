2c87295e6afb9760fa743c6f1b762f511d37e380
Artem Pokamestov <temka.pokamestov@mail.ru>
2021-03-15 23:26:08 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginForm
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_closed_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Header_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void LogoContainer_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(left_grid.Visibility == Visibility.Hidden)
+            {
+                left_grid.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                left_grid.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (grid_right.Visibility == Visibility.Hidden)
+            {
+                grid_right.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                grid_right.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (tb2.Password.Length >0)
+            {
+                waterMark.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                waterMark.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
